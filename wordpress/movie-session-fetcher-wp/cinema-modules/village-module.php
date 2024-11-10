f31641689ee3a2fe7ff9bfa28d9d7489ddd5e0c2
Evan Kim <s3943715@student.rmit.edu.au>
2024-11-10 13:03:10 +1100

    started village movie import

--- wordpress/movie-session-fetcher-wp/cinema-modules/village-module.php
+++ wordpress/movie-session-fetcher-wp/cinema-modules/village-module.php
@@ -42,11 +42,64 @@
         }
     }
 
-    function village_fetch_and_insert_movies($village_session_id) {
+    function village_get_all_movies($village_session_id) {
 
         // Increase time limit to 5 minutes for this operation
         set_time_limit(600); 
         
+        //api endpoints
+        $api_url_now_showing = 'https://villagecinemas.com.au/api/film/getMoviesNowShowing?userSessionId=';
+        $api_url_coming_soon = 'https://villagecinemas.com.au/api/film/getMoviesComingSoon?userSessionId=';
+
+    }
+
+    function village_get_movies_from_api($api_url){
+
+        // Get JSON from given API
+        $response = wp_remote_get($api_url);
+
+        // Error Check
+        if (is_wp_error($response)) {
+            error_log("Village Module: Failed to retrieve movies from " . $api_url);
+            return;
+        }
+
+        //Decode JSON
+        $movies = json_decode( wp_remote_retrieve_body( $response ), true);
+
+        // Empty Check
+        if (empty($movies)) {
+            error_log("Village Module: No movies found in the response from " . $api_url);
+            return;
+        }
+
+        // Loop thorugh movies
+        foreach ($movies as $movie) {
+            $movie_title = sanitize_text_field( $movie['Title'] );
+
+            // Use WP_Query to check if a post with the same movie title exists
+            $args = array(
+                'post_type'  => 'Movie', // Custom post type
+                'title'      => $movie_title,
+                'posts_per_page' => 1 // Limit to 1 result
+            );
+            $query = new WP_Query( $args );
+        }
+
+        // village id to exisitng post
+        if ( $query->have_posts() ) {
+            // If the movie already exists, update its status
+            $existing_movie_id = $query->posts[0]->ID;        
+
+            // Update the status taxonomy of the existing movie
+            update_post_meta( $existing_movie_id, 'VillageID', sanitize_text_field( $movie['MovieId'] ) ); // Store vistaId as HoytsID
+
+            wp_reset_postdata();
+            continue;
+        }
+
+        //ADD NEW MOVIE POST CODE HERE
+
     }
     
     // ********************************************************************************
@@ -61,7 +114,7 @@
     add_action( 'fetch_sessions', 'village_fetch_and_insert_sessions_all_venues' );
     
     // IN: $venue_code, $state, $suburb -> in array above called 'venues'.
-    function village_fetch_and_insert_sessions($village_session_id) {
+    function village_fetch_and_insert_sessions($village_session_id, $venue_code, $state, $suburb) {
         return;
     }
 ?>
